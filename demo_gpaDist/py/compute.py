d3ac102e1d8f88288d585ff64529aa1dcfd221ef
Wade Fagen-Ulmschneider (waf) <waf@illinois.edu>
2016-09-13 10:39:24 -0500

    demo_gpaDist

---
+++ demo_gpaDist/py/compute.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+# Read the CSV file into data, by row
+with open("res/fa2014.csv") as f:
+    reader = csv.DictReader(f)
+    data = [row for row in reader]
+
+# Write your code here
+courses = {}
+
+
+
+
+# Write the courses dictionary to `res/gpa.json`
+with open("res/gpa.json", "w") as f:
+    json.dump(courses, f, indent=2)
