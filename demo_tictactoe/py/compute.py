c499607d5c1427a6376567c86b966db35aea984e
Wade Fagen-Ulmschneider (waf) <waf@illinois.edu>
2016-10-18 10:50:10 -0500

    demo_tictactoe

---
+++ demo_tictactoe/py/compute.py
@@ -0,0 +1,12 @@
+import json
+import networkx as nx
+
+def getBoardState(board):
+    return 0
+
+def addNextBoards(G, board, turn):
+    return 0
+
+getBoardState(["X", "X", "O", "X", "O", "-", "O", "O", "X"])
+getBoardState(["X", "O", "X", "O", "X", "O", "-", "-", "-"])
+getBoardState(["X", "X", "-", "O", "O", "-", "X", "O", "-"])
