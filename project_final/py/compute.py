2d59e7a8ac8de640bf6be8719d8e0e90a3699370
Wade Fagen-Ulmschneider (waf) <waf@illinois.edu>
2016-12-01 09:25:49 -0600

    project_final

---
+++ project_final/py/compute.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+# Reading a CSV file:
+#
+#with open("res/fileName.csv") as f:
+#    reader = csv.DictReader(f)
+#    data = [row for row in reader]
+
+
+# Writing to a JSON file:
+#
+#with open("res/fileName.json", "w") as f:
+#    outdata = json.dumps(data, indent=2)
+#    f.write(outdata)
